db3a85df139fa46e5257100f41a731f680caf68e
Fernando Almeida <nando.a.g.martines@gmail.com>
2023-05-22 12:15:52 -0300

    rails new

--- app/controllers/pagescd_controller.rb
+++
@@ -1,4 +0,0 @@
-class PagescdController < ApplicationController
-  def /home/fernandoalmeida/code/nandoalmeidam
-  end
-end
--- test/controllers/pagescd_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class PagescdControllerTest < ActionDispatch::IntegrationTest
-  test "should get /home/fernandoalmeida/code/nandoalmeidam" do
-    get pagescd_/home/fernandoalmeida/code/nandoalmeidam_url
-    assert_response :success
-  end
-end
